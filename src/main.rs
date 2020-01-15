d59653f14c14b0a33e48ca34b2202c873795ecf8
nouman-96 <noumanqadri786@gmail.com>
2020-01-15 21:40:40 +0500

    Assignment_07 Task_01

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+pub mod My_life{
+    pub mod Life_goals {
+        pub fn Will_power(){
+            println!("The purpose of life should be the life of purpose");
+        }
+    }
+} 
+
+fn main() {
+    My_life::Life_goals::Will_power();
+    
+}
